5daec81d5eaeb7c073c181c751454666da501017
sksrimant <sksrimant@gmail.com>
2016-06-14 12:10:47 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL
+        
+		set <- function(z) {
+                x <<- z
+                inv <<- NULL
+        }
+        get <- function() x
+        
+		setInverse <- function(inverse) inv <<- inverse			
+        
+		getInverse <- function() inv
+        
+		list(set = set,get = get,setInverse = setInverse,getInverse = getInverse)
 }
 
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+		inv <- x$getInverse()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        mat_value <- x$get()
+        inv <- solve(mat_value,...)
+        x$setInverse(inv)
+        inv
+
+		}
